5b4b6a4c482bbd30f744280247643faca9d46cc1
george <heorhii.kyryliuk@gmail.com>
2025-08-23 18:18:43 +0300

    base setup files done

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import React, { useEffect } from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  useEffect(() => {
-    isDarkMode;
-  }, [isDarkMode]);
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.container}>
-        <Text style={styles.text}>Welcome Native!</Text>
-        <Text style={styles.second}>
-          I am testing this app right now lets see what happens
-        </Text>
-        <Text style={styles.third}>One more test</Text>
-        <Text style={styles.text}>Great built!</Text>
-      </View>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome Native!</Text>
+    </View>
   );
 };
 
@@ -35,22 +14,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#333333',
   },
   text: {
     fontSize: 24,
     fontWeight: '600',
-    color: 'black',
-  },
-  second: {
-    textAlign: 'center',
-    fontSize: 32,
-    fontWeight: 800,
-    color: 'green',
-  },
-  third: {
-    textAlign: 'center',
-    fontSize: 21,
-    fontWeight: 500,
     color: 'blue',
   },
 });
